b8596dbf738032e6104906fd689af62ecc5d2ba7
Philippe Damerval <damerval@gmail.com>
2016-11-30 20:36:47 -0900

    interim dev

--- js/admin_data.js
+++ js/admin_data.js
@@ -5,12 +5,12 @@
 var substitutesGridSource = {
   url: 'helpers/substitutesData.php',
   dataFields: [
-    { name: 'ProgramKey', type: 'int' },
-    { name: 'Name' },
-    { name: 'Program' },
-    { name: 'Position' },
-    { name: 'StartDate', map: 'StartDate>date', type: 'date' },
-    { name: 'EndDate', map: 'EndDate>date', type: 'date' }
+    {name: 'ProgramKey', type: 'int'},
+    {name: 'Name'},
+    {name: 'Program'},
+    {name: 'Position'},
+    {name: 'StartDate', map: 'StartDate>date', type: 'date'},
+    {name: 'EndDate', map: 'EndDate>date', type: 'date'}
   ],
   dataType: 'json',
   id: 'ProgramKey',
@@ -20,21 +20,21 @@ var substitutesGridSource = {
 var substituteGridAdapter = new $.jqx.dataAdapter(substitutesGridSource);
 
 var substitutesColumns = [
-  { text: "Employee", dataField: 'Name', width: 150 },
-  { text: "Program", dataField: 'Program', width: 222 },
-  { text: "Position", dataField: 'Position', width: 312 },
-  { text: "Start Date", dataField: 'StartDate', width: 100, cellsformat: 'dd/MM/yyyy' },
-  { text: "End Date", dataField: 'EndDate', width: 100, cellsformat: 'dd/MM/yyyy' }
+  {text: "Employee", dataField: 'Name', width: 150},
+  {text: "Program", dataField: 'Program', width: 222},
+  {text: "Position", dataField: 'Position', width: 312},
+  {text: "Start Date", dataField: 'StartDate', width: 100, cellsformat: 'dd/MM/yyyy'},
+  {text: "End Date", dataField: 'EndDate', width: 100, cellsformat: 'dd/MM/yyyy'}
 ];
 
 var jobClassRolesSource = {
   url: "helpers/jobClassRoles.php",
   dataType: 'json',
   dataFields: [
-    { name: 'id', type: 'int' },
-    { name: 'prg', type: 'string' },
-    { name: 'jobClass', type: 'string' },
-    { name: 'grp', type: 'string' }
+    {name: 'id', type: 'int'},
+    {name: 'prg', type: 'string'},
+    {name: 'jobClass', type: 'string'},
+    {name: 'grp', type: 'string'}
   ],
   id: 'id',
   pageSize: 14
@@ -43,28 +43,28 @@ var jobClassRolesSource = {
 var jobClassRolesAdapter = new $.jqx.dataAdapter(jobClassRolesSource);
 
 var jobClassRolesColumns = [
-  { text: "Program", dataField: 'prg', width: 200 },
-  { text: "Job Class", dataField: 'jobClass', width: 228 },
-  { text: "Permissions group", dataField: 'grp', width: 200 }
+  {text: "Program", dataField: 'prg', width: 200},
+  {text: "Job Class", dataField: 'jobClass', width: 228},
+  {text: "Permissions group", dataField: 'grp', width: 200}
 ];
 
 var individualSource = {
   url: "helpers/individualRoles.php",
   dataType: 'json',
   dataFields: [
-    { name: 'userNum', type: 'int' },
-    { name: 'grp', type: 'string' },
-    { name: 'startDate', map: 'startDate>date', type: 'date' },
-    { name: 'endDate', map: 'endDate>date', type: 'date' }
+    {name: 'userNum', type: 'int'},
+    {name: 'grp', type: 'string'},
+    {name: 'startDate', map: 'startDate>date', type: 'date'},
+    {name: 'endDate', map: 'endDate>date', type: 'date'}
   ],
   id: 'ugId'
 };
 
 var individualColumns = [
-  { text: 'User Name', dataField: 'userNum' },
-  { text: 'Group', dataField: 'grp' },
-  { text: 'startDate', dataField: 'startDate', cellsFormat: 'MM/dd/yyyy' },
-  { text: 'endDate', dataField: 'endDate', cellsFormat: 'MM/dd/yyyy' }
+  {text: 'User Name', dataField: 'userNum'},
+  {text: 'Group', dataField: 'grp'},
+  {text: 'startDate', dataField: 'startDate', cellsFormat: 'MM/dd/yyyy'},
+  {text: 'endDate', dataField: 'endDate', cellsFormat: 'MM/dd/yyyy'}
 ];
 
 var individualAdapter = new $.jqx.dataAdapter(individualSource);
@@ -73,19 +73,19 @@ var exclusionsSource = {
   url: "helpers/exclusions.php",
   dataType: 'json',
   dataFields: [
-    { name: 'eid', type: 'int' },
-    { name: 'cid', type: 'int' },
-    { name: 'exStart', map: 'exStart>date', type: 'date' },
-    { name: 'exEnd', map: 'exEnd>date', type: 'date' }
+    {name: 'eid', type: 'int'},
+    {name: 'cid', type: 'int'},
+    {name: 'exStart', map: 'exStart>date', type: 'date'},
+    {name: 'exEnd', map: 'exEnd>date', type: 'date'}
   ],
   id: 'uid'
 };
 
 var exclusionsColumns = [
-  { text: "Employee", dataField: 'eid' },
-  { text: "Client", dataField: 'cid' },
-  { text: "Start", dataField: 'exStart', cellsFormat: 'MM/dd/yyyy' },
-  { text: "End", dataField: 'exEnd', cellsFormat: 'MM/dd/yyyy' }
+  {text: "Employee", dataField: 'eid'},
+  {text: "Client", dataField: 'cid'},
+  {text: "Start", dataField: 'exStart', cellsFormat: 'MM/dd/yyyy'},
+  {text: "End", dataField: 'exEnd', cellsFormat: 'MM/dd/yyyy'}
 ];
 
 var exclusionsAdapter = new $.jqx.dataAdapter(exclusionsSource);
@@ -94,27 +94,53 @@ var userAccessSource = {
   url: "helpers/userAccess.php",
   dataType: 'json',
   dataFields: [
-    { name: 'hrID', type: 'int' },
-    { name: 'firstName', map: 'fn' },
-    { name: 'lastName', map: 'ln' },
-    { name: 'emiID', type: 'int' },
-    { name: 'jysID', type: 'int' },
-    { name: 'userLogin', map: 'ul' },
-    { name: 'userJYSNumber', map: 'un', type: 'int' },
-    { name: 'sqlLogin', map: 'sl' },
-    { name: 'emiDuplicates', map: 'dupes', type: 'int' }
+    {name: 'hrID', type: 'int'},
+    {name: 'firstName', map: 'fn'},
+    {name: 'lastName', map: 'ln'},
+    {name: 'emiID', type: 'int'},
+    {name: 'jysID', type: 'int'},
+    {name: 'userLogin', map: 'ul'},
+    {name: 'userJYSNumber', map: 'un', type: 'int'},
+    {name: 'sqlLogin', map: 'sl'},
+    {name: 'emiDuplicates', map: 'dupes', type: 'int'}
   ],
-  id: 'hrID'
+  id: 'hrID',
+  pagesize: 14
 };
 
 var userAccessColumns = [
-  { text: "HR ID", dataField: 'hrID' },
-  { text: "First Name", dataField: 'firstName' },
-  { text: "Last Name", dataField: 'lastName' },
-  { text: "EMI ID", dataField: 'emiID' },
-  { text: "JYS ID", dataField: 'jysID' },
-  { text: "User Access #", dataField: 'userJYSNumber' },
-  { text: "SQL Login GUID", dataField: 'sqlLogin' },
-  { text: "User Login", dataField: 'userLogin' },
-  { text: "EMI Dupes", dataField: 'emiDuplicates' }
+  {text: "HR ID", dataField: 'hrID', width: 50},
+  {text: "First Name", dataField: 'firstName'},
+  {text: "Last Name", dataField: 'lastName'},
+  {text: "EMI ID", dataField: 'emiID'},
+  {text: "JYS ID", dataField: 'jysID'},
+  {text: "User Access #", dataField: 'userJYSNumber'},
+  {text: "SQL Login GUID", dataField: 'sqlLogin'},
+  {text: "User Login", dataField: 'userLogin'},
+  {text: "EMI Dupes", dataField: 'emiDuplicates'}
 ];
+
+var userAccessAdapter = new $.jqx.dataAdapter(userAccessSource);
+
+
+var btnNewSubstitute;
+var btnEditSubstitute;
+var btnEndSubstitute;
+var btnDeleteSubstitute;
+
+var btnNewExclusion;
+var btnEditExclusion;
+var btnDeleteExclusion;
+
+var btnNewJobClassPermission;
+var btnEditJobClassPermission;
+var btnDeleteJobClassPermission;
+
+var btnNewIndividualPermission;
+var btnEditIndividualPermission;
+var btnDeleteIndividualPermission;
+
+var btnGrantUserAccess;
+var btnDenyUserAccess;
+var btnResetPassword;
+var btnEMIwindow;
--- js/admin_widgets.js
+++ js/admin_widgets.js
@@ -17,17 +17,17 @@ $(document).ready( function() {
   var tabs = $("#tabs");
   tabs.jqxTabs({ width: 897, height: 525, position: 'top', theme: 'jys' });
 
-  var btnNewSubstitute = $("#addSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnEditSubstitute = $("#editSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnEndSubstitute = $("#endSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnDeleteSubstitute = $("#deleteSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
+  btnNewSubstitute = $("#addSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEditSubstitute = $("#editSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEndSubstitute = $("#endSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
+  btnDeleteSubstitute = $("#deleteSubstituteButton").jqxButton({ width: 150, theme: 'jys' });
 
   var pnlSubstitutesButtons = $("#substitutesButtonBar");
   pnlSubstitutesButtons.jqxPanel({ width: 876, height: 30, theme: 'jys' });
 
-  var btnNewJobClassPermission = $("#addJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnEditJobClassPermission = $("#editJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnDeleteJobClassPermission = $("#deleteJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnNewJobClassPermission = $("#addJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEditJobClassPermission = $("#editJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnDeleteJobClassPermission = $("#deleteJobClassPermissionButton").jqxButton({ width: 150, theme: 'jys' });
 
   var pnlJobClassPermissionsButtons = $("#positionPermissionsButtonBar");
   pnlJobClassPermissionsButtons.jqxPanel({ width: 620, height: 30, theme: 'jys' });
@@ -53,9 +53,9 @@ $(document).ready( function() {
   var pnlIndividualPermissionsButtons = $("#individualPermissionsButtonBar");
   pnlIndividualPermissionsButtons.jqxPanel({ width: 620, height: 30, theme: 'jys' });
 
-  var btnNewIndividualPermission = $("#addIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys '});
-  var btnEditIndividualPermission = $("#editIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys '});
-  var btnDeleteIndividualPermission = $("#deleteIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys '});
+  btnNewIndividualPermission = $("#addIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEditIndividualPermission = $("#editIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnDeleteIndividualPermission = $("#deleteIndividualPermissionButton").jqxButton({ width: 150, theme: 'jys' });
 
   var exclusionsGrid = $("#exclusionsGrid");
   exclusionsGrid.jqxGrid({
@@ -68,8 +68,23 @@ $(document).ready( function() {
   var pnlExclusionsButtons = $("#exclusionsButtonBar");
   pnlExclusionsButtons.jqxPanel({ width: 620, height: 30, theme: 'jys' });
 
-  var btnNewExclusion = $("#addExclusionButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnEditExclusion = $("#editExclusionButton").jqxButton({ width: 150, theme: 'jys' });
-  var btnDeleteExclusion = $("#deleteExclusionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnNewExclusion = $("#addExclusionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEditExclusion = $("#editExclusionButton").jqxButton({ width: 150, theme: 'jys' });
+  btnDeleteExclusion = $("#deleteExclusionButton").jqxButton({ width: 150, theme: 'jys' });
+
+  var pnlUserAccessButtons = $("#userAccessButtonBar");
+  pnlUserAccessButtons.jqxPanel({ width: 876, height: 30, theme: 'jys' });
+
+  var userAccessGrid = $("#userAccessGrid");
+  userAccessGrid.jqxGrid({
+    width: 885, height: 446, theme: 'jys', altrows: true, columnsHeight: 18,
+    source: userAccessAdapter, columns: userAccessColumns,
+    sortable: true, pageable: true, pagerMode: 'simple'
+  });
+
+  btnGrantUserAccess = $("#grantAccessButton").jqxButton({ width: 150, theme: 'jys' });
+  btnDenyUserAccess = $("#denyAccessButton").jqxButton({ width: 150, theme: 'jys' });
+  btnResetPassword = $("#resetPasswordButton").jqxButton({ width: 150, theme: 'jys' });
+  btnEMIwindow = $("#EMIButton").jqxButton({ width: 150, theme: 'jys' });
 
 });
